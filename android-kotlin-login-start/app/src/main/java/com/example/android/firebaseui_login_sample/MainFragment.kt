1aeb5315eeda28147fea7d6fb41733472b046d7f
Juan Estrella <jlkmoy@gmail.com>
2022-01-23 19:22:12 -0500

    feat: android-kotlin-login-start: completed

--- android-kotlin-login-start/app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
+++ android-kotlin-login-start/app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
@@ -62,6 +62,12 @@ class MainFragment : Fragment() {
 
         binding.authButton.setOnClickListener {
             // TODO call launchSignInFlow when authButton is clicked
+            launchSignInFlow()
+        }
+
+        binding.settingsBtn.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToSettingsFragment()
+            findNavController().navigate(action)
         }
     }
 
@@ -70,6 +76,16 @@ class MainFragment : Fragment() {
         // TODO Listen to the result of the sign in process by filter for when
         //  SIGN_IN_REQUEST_CODE is passed back. Start by having log statements to know
         //  whether the user has signed in successfully
+        val response = IdpResponse.fromResultIntent(data)
+        if (requestCode == SIGN_IN_RESULT_CODE){
+            //   User successfully signed in
+            Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}")
+        } else {
+            // Signed in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+        }
     }
 
     /**
@@ -82,14 +98,29 @@ class MainFragment : Fragment() {
 
         // TODO Use the authenticationState variable from LoginViewModel to update the UI
         //  accordingly.
-        //
-        //  TODO If there is a logged-in user, authButton should display Logout. If the
-        //   user is logged in, you can customize the welcome message by utilizing
-        //   getFactWithPersonalition(). I
-
-        // TODO If there is no logged in user, authButton should display Login and launch the sign
-        //  in screen when clicked. There should also be no personalization of the message
-        //  displayed.
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when(authenticationState){
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    binding.authButton.text = getString(R.string.logout_button_text)
+                    binding.authButton.setOnClickListener{
+                        // TODO implement logging out user
+                        AuthUI.getInstance().signOut(requireContext())
+                    }
+                    //  TODO If there is a logged-in user, authButton should display Logout. If the
+                    //   user is logged in, you can customize the welcome message by utilizing
+                    //   getFactWithPersonalition().
+                    binding.welcomeText.text = getFactWithPersonalization(factToDisplay)
+                }
+                else -> {
+                    // TODO If there is no logged in user, authButton should display Login and launch the sign
+                    //  in screen when clicked. There should also be no personalization of the message
+                    //  displayed.
+                    binding.authButton.text = getString(R.string.login_button_text)
+                    binding.authButton.setOnClickListener { launchSignInFlow() }
+                    binding.welcomeText.text = factToDisplay
+                }
+            }
+        })
     }
 
 
@@ -106,5 +137,25 @@ class MainFragment : Fragment() {
     private fun launchSignInFlow() {
         // TODO Complete this function by allowing users to register and sign in with
         //  either their email address or Google account.
+        // Give users the option to sign in / register with their email or Google account.
+        // If users choose to register with their email,
+        // they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+
+            // This is where you can provide more ways for users to register and
+            // sign in.
+        )
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            MainFragment.SIGN_IN_RESULT_CODE
+        )
+
     }
 }
